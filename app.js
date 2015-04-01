0fad3d802ab14f8ca3fb0401628e05b9558cc5ab
Toon <borgers.toon@gmail.com>
2015-04-01 16:02:31 +0200

    Added mongo deps, seperate routing file

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+require('./routes/routes')(app);
 
 var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+    console.log('Example app listening at http://%s:%s', host, port);
 });
